8f361a225d712aca33f9874cd6ee72fb84c37c5f
vic778 <victoiremmanuelbarh@gmail.com>
2021-11-27 13:49:33 +0200

    Added background color in the css file

---
+++ src/hello.js
@@ -0,0 +1,11 @@
+const vic = () => {
+    console.log('hi vic');
+}
+
+export {vic};
+
+const merva = () => {
+    console.log('i love u');
+}
+
+export {merva};
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {vic} from './hello';
+import {merva} from './hello';
+import './styles.css';
+
+
+merva();
+vic();
+vic();
+vic();
+merva();
+merva();
+merva();
+
+
+console.log('hello webpack');
