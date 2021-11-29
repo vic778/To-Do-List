0acb339ebc2d073894508dd3e02f9c5696927947
vic778 <victoiremmanuelbarh@gmail.com>
2021-11-29 09:45:22 +0200

    Fixed all linters errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: "development",
+  mode: 'development',
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -12,19 +12,19 @@ module.exports = {
     rules: [
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
         options: {
-            minimize: true
-        }
+          minimize: true,
+        },
       },
-       {
+      {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: "index.html",
+    template: path.resolve(__dirname, './src/index.html'),
+    filename: 'index.html',
   })],
 };
