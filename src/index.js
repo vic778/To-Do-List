ad2e9b49f89cbef547bf681c5ba27c57d0092e31
vic778 <victoiremmanuelbarh@gmail.com>
2021-11-29 08:51:50 +0200

    Creatd a constructor to stock all list inside the array

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-import * as vic from './hello';
-import * as merva from './hello';
 import './styles.css';
+import addList from './addToList';
 
-merva();
-vic();
-vic();
-vic();
-merva();
-merva();
-merva();
+const todoList = document.querySelector('#todoList');
+const inputFaild = document.querySelector('#inputFaild');
+// const clearAll = document.querySelector('#clearAll');
 
-console.log('hello webpack');
+const stock = [];
+
+function displayToDoList() {
+  const data = localStorage.getItem('stock');
+  if (data) {
+    JSON.parse(data).forEach((todo) => {
+      const li = document.createElement('li');
+      li.classList.add('list-group-item');
+      const text = `<div class="list-todo" id="${todo.index}">
+    <input class="form-check-input" type="checkbox" value="" ${todo.completed ? 'checked' : ''} id="defaultCheck1"/>
+          <div class="description ${todo.completed ? 'checked' : ''}" contenteditable="${!todo.completed}">${todo.description}</div>
+          <i class="fas fa-trash delete"></i>
+       </div>`;
+      li.innerHTML = text;
+      todoList.appendChild(li);
+    });
+  }
+}
+
+inputFaild.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && inputFaild.value) {
+    addList(stock, inputFaild.value);
+    todoList.innerHTML = '';
+    displayToDoList();
+    inputFaild.value = '';
+  }
+});
+
+// const editButtons = document.querySelectorAll('.description');
+// editButtons.forEach((btn) => {
+//   btn.addEventListener('keypress', (e) => {
+//     if (e.key === 'Enter') {
+//       editTodo(e);
+//       todoList.innerHTML = '';
+//       displayToDoList();
+//     }
+//   });
+// });
